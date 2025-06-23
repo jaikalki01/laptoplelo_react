900a0f6dcd449b848c2ca5713c57e8d59788abd8
JINCY <mathewjincy1@gmail.com>
2025-06-23 12:49:24 +0530

    23

--- src/pages/SignupPage.tsx
+++ src/pages/SignupPage.tsx
@@ -63,7 +63,7 @@ const SignupPage = () => {
     setIsLoading(true);
 
     try {
-      const response = await fetch("http://127.0.0.1:8001/users/", {
+      const response = await fetch("http://127.0.0.1:8001/users/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
